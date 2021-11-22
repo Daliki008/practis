e918ee645ca49c2516d6a885896f08479d2df573
Daliki008 <93877290+Daliki008@users.noreply.github.com>
2021-11-23 00:33:59 +0600

    Add files via upload

---
+++ Калкабаев_практика/src/components/main-page/v-main-page.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='v-main-page'>
+    <h1>Vypolnen</h1>
+    <p>
+      <router-link :to="{name: 'catalog'}">
+        by Bykov
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "v-main-page",
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
